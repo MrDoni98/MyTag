438fcb82d8dc468bb7382d7c593641ddb8f19822
Gamecrafter <pro_victor@ymail.com>
2015-02-28 13:50:42 -0800

    Prepare for sub-commands

--- src/MyTag/Loader.php
+++ src/MyTag/Loader.php
@@ -25,78 +25,9 @@ class Loader extends PluginBase implements Listener{
     
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         switch($command->getName()){
-            case "healthtag":
-                if($sender instanceof Player){
-                    $health = $sender->getHealth();
-                    $maxhealth = $sender->getMaxHealth();
-                    $name = $sender->getName();
-                    $sender->setNameTag("$name $health/$maxhealth");
-                    $sender->sendMessage("Your health is now publicly visible.");
-                    return true;
-                }
-                else{
-                    $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
-                    return true;
-                }
-            break;
-            case "hidetag":
-                if($sender instanceof Player){
-                    $sender->setNameTag("");
-                    $sender->sendMessage("Incognito mode enabled!");
-                    return true;
-                }
-                else{
-                    $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
-                    return true;
-                }
-            break;
-            case "iptag":
-                if($sender instanceof Player){
-                    $ip = $sender->getAddress();
-                    $name = $sender->getName();
-                    $sender->setNameTag("$name\n$ip");
-                    return true;
-                }
-                else{
-                    $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
-                    return true;
-                }
-            break;
-            case "nametag":
-                if($sender instanceof Player){
-                    $name = $sender->getName();
-                    $sender->setNameTag("$name");
-                    $sender->sendMessage("Your default tag has been restored.");
-                    return true;
-                }
-                else{
-                    $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
-                    return true;
-                }
-            break;
-            case "optag":
-                if($sender instanceof Player){
-                    if($sender->isOp()){
-                        $name = $sender->getName();
-                        $sender->setNameTag("[OP] $name");
-                        $sender->sendMessage("Your op status is now publicly visible.");
-                        return true;
-                    }
-                    else{
-                        $sender->sendMessage("You are not an op!");
-                        return true;
-                    }
-                }
-                else{
-                    $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
-                    return true;
-                }
-            break;
             case "tag":
                 if($sender instanceof Player){
-                    $tag = $sender->getNameTag();
-                    $sender->sendMessage("Your tag:\n$tag");
-                    return true;
+
                 }
                 else{
                     $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
