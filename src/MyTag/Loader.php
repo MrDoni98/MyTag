dc298e930a73ff459702841c6096f683cb9a7316
Gamecrafter <pro_victor@ymail.com>
2015-02-28 21:45:08 -0800

    Added part of the code

--- src/MyTag/Loader.php
+++ src/MyTag/Loader.php
@@ -27,7 +27,64 @@ class Loader extends PluginBase implements Listener{
         switch($command->getName()){
             case "tag":
                 if($sender instanceof Player){
-
+                    $sub = array_shift($args);
+                    switch($sub){
+                        case "":
+                        case "help":
+                            $sender->sendMessage(TextFormat::WHITE."/tag address");
+                            $sender->sendMessage(TextFormat::WHITE."/tag health");
+                            $sender->sendMessage(TextFormat::WHITE."/tag hide");
+                            $sender->sendMessage(TextFormat::WHITE."/tag money");
+                            $sender->sendMessage(TextFormat::WHITE."/tag op");
+                            $sender->sendMessage(TextFormat::WHITE."/tag restore");
+                            $sender->sendMessage(TextFormat::WHITE."/tag show");
+                            return true;
+                        break;
+                        case "address":
+                            $ip = $sender->getAddress();
+                            $port = $sender->getPort();
+                            $tag = $sender->getNameTag();
+                            $sender->setNameTag("$tag $ip:$port");
+                            return true;
+                        break;
+                        case "health":
+                            $health = $sender->getHealth();
+                            $maxhealth = $sender->getMaxHealth();
+                            $tag = $sender->getNameTag();
+                            $sender->setNameTag("$tag $health/$maxhealth");
+                            return true;
+                        break;
+                        case "hide":
+                            $sender->setNameTag("");
+                            $sender->sendMessage("Incognito mode enabled!");
+                            return true;
+                        break;
+                        case "money":
+                            $tag = $sender->getNameTag();
+                            return true;
+                        break;
+                        case "op":
+                            if($sender->isOp()){
+                                $tag = $sender->getNameTag();
+                                $sender->setNameTag("[OP] $tag");
+                                return true;
+                            }
+                            else{
+                                $sender->sendMessage("You are not an op.");
+                                return true;
+                            }
+                        break;
+                        case "restore":
+                            $name = $sender->getName();
+                            $sender->setNameTag($name);
+                            $sender->sendMessage("Your default name tag has been restored.");
+                            return true;
+                        break;
+                        case "show":
+                            $sender->setNameTag("Incognito mode disabled!");
+                            return true;
+                        break;
+                    }
                 }
                 else{
                     $sender->sendMessage(TextFormat::RED."Please run this command in-game.");
@@ -46,8 +103,7 @@ class Loader extends PluginBase implements Listener{
     }
 
     public function onPlayerQuit(PlayerQuitEvent $event){
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        $player->setNameTag("$name");
+        $name = $event->getPlayer()->getName();
+        $event->getPlayer()->setNameTag($name);
     }
 }
